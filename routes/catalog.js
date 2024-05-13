9f70f432c825a0c7642851a9843518035da4ab8b
Optimus115 <91112019+Optimus115@users.noreply.github.com>
2024-05-13 15:33:55 +0530

    devlopin all kind off routes

---
+++ routes/catalog.js
@@ -0,0 +1,16 @@
+var express = require('express');
+const  {movies} = require("../public/javascripts/movies");
+var router = express.Router();
+// get the home page
+
+router .post('/' ,function(req,res,next){
+   res.render('catalog', {
+    title:'Movie Ticket Demo for Node.js', movies: movies 
+   });
+});
+
+router.get('/', function(req, res, next) {
+    res.render('catalog', { title: 'Movie Ticket Demo for Node.js', movies: movies });
+  });
+  
+  module.exports = router;
---
+++ routes/welcome.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+var bodyPraser = require('body-parser');
+var catalogRouter = require('./routes/catalog');
+require('dotenv').config();
+const {sendMessage , getMessageInput} = require('../messageHelper');
+app.use ('catalog', catalogRouter);
+
+router.use(bodyPraser.json());
+router.post('/', function(req ,res, next){
+    var data = getMessageInput(process.env.RECIPIENT_WAID, 'Welcome to the Movie Ticket Demo App for Node.js!');
+
+    sendMessage(data).then(function(response){
+        res.redirect('/catalog');
+        res.sendStatus(200);
+        return;
+    })
+    .catch(function(error){
+        console.log(error);
+        console.log(error.response.data);
+        res.sendStatus(500);
+        return;
+    });
+});
+
+module.exports = router;
